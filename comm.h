8d1411aacfbc3813845fd698e7a1e77e7b6d7a24
liy <liy8272@gmail.com>
2025-02-13 09:37:43 +0800

    Remove unnecessary define

--- comm.h
+++ comm.h
@@ -9,15 +9,6 @@
 
 #pragma once
 
-/* ESPNOW can work in both station and softap mode. It is configured in menuconfig. */
-#if CONFIG_ESPNOW_WIFI_MODE_STATION
-#define ESPNOW_WIFI_MODE WIFI_MODE_STA
-#define ESPNOW_WIFI_IF   ESP_IF_WIFI_STA
-#else
-#define ESPNOW_WIFI_MODE WIFI_MODE_AP
-#define ESPNOW_WIFI_IF   ESP_IF_WIFI_AP
-#endif
-
 #define ESPNOW_QUEUE_SIZE           6
 
 // Broadcast MAC address
